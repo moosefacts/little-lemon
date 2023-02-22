22dc6e0601995f5f5e65c12e4868ff72b1f67496
Chris <116923904+moosefacts@users.noreply.github.com>
2023-02-21 18:00:08 -0800

    css styling added

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
+
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Nav from "./components/Nav";
+import Highlights from "./components/Highlights";
+import Testimonials from "./components/Testimonials";
+import About from "./components/About";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Abc
-    </div>
+
+
+    <main class="container">
+      <div class="nav">
+        <Nav/>
+      </div>
+      <div class="hero">
+        <Hero/>
+      </div>
+      <div class="highlights">
+        <Highlights/>
+      </div>
+      <div class="testimonials">
+        <Testimonials/>
+      </div>
+      <div class="about">
+        <About/>
+      </div>
+      <div class="footer">
+        <Footer/>
+      </div>
+    </main>
   );
 }
 
